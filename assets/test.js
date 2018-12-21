6cdd8d8be3331d173b94d2d9b68b37b5d41c6345
87duran <cduran@oozlemedia.com>
2018-12-21 15:59:49 -0700

    final commit

--- assets/test.js
+++ assets/test.js
@@ -2,10 +2,15 @@ $( document ).ready(function() {
     console.log( "ready!" );
 });
 $('form#sidebar-form').submit( function(event) {
-  if (1 === 1) {
-    alert('try again!');
+  event.preventDefault();
+  var fieldOne = $('#field-1').val().trim();
+  var fieldTwo = $('#field-2').val().trim();
+  var fieldThree = $('#field-3').val().trim();
+  var fieldFour = $('#field-4').val().trim();
+  console.log(fieldOne);
+  if ((fieldOne === "") || (fieldTwo === "") || (fieldThree === "") || (fieldFour === "")) {
+    alert('Please fill out every form field.');
   } else {
-    alert('submitted!');
-    event.preventDefault();
+    $('#form-submitted').modal();
   }
 })
