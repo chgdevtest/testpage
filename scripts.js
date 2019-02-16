fa028e3c2e1c9c40068d51ea6f71cc1863af2e2b
Brian Barrow <briancbarrow@gmail.com>
2019-02-16 16:03:39 -0700

    removed HTML validation, added basic Vue.js validation

--- scripts.js
+++ scripts.js
@@ -1,10 +1,34 @@
 const vueApp = new Vue({
   el: "#app",
   data: {
-    showModal: true,
-    test: "Hello world"
+    showModal: false,
+    firstName: "",
+    lastName: "",
+    phone: "",
+    email: "",
+    comments: "",
+    errors: []
   },
   methods: {
+    checkForm() {
+      this.errors = [];
+      if (!this.firstName) {
+        this.errors.push("First Name is required");
+      }
+      if (!this.lastName) {
+        this.errors.push("Last Name is required");
+      }
+      if (!this.phone) {
+        this.errors.push("Phone is required");
+      }
+      if (!this.email) {
+        this.errors.push("Email is required");
+      }
+
+      if (this.errors.length === 0) {
+        this.submitForm();
+      }
+    },
     submitForm() {
       this.showModal = true;
     },
